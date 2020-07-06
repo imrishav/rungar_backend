60fab43994a8921c6940ef229f0554f11a08eba2
Rishav Sinha <rishav@live.co.uk>
2020-07-07 01:27:13 +0530

    Added New Items Queryy

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    //Todo check if user is logged in
+
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    return item;
+  },
+};
 
 module.exports = Mutation;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db'),
+
+  // async items(parent, args, ctx, info) {
+  //   console.log('asdasd', await ctx.db.query);
+  //   const item = await ctx.db.query.items();
+
+  //   //we will api request here to the db
+
+  //   return item;
+  // },
+};
 
 module.exports = Query;
